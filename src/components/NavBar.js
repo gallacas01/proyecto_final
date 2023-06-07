492b7fe9f3f62289c1ee6959c11d5d12d933653b
Miguel Gallardo Castillo <mgalcas872@gmail.com>
2023-06-07 18:32:49 +0200

    Inicio responsive

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -75,7 +75,7 @@ function BarraNavegacion({ datosNavBar }) {
                     <ul className="navbar-nav me-auto mb-lg-0 p-2">
                         {listaNavItem}
                         <li className='nav-item'>
-                            <button className='p-0 mt-2 fs-4' onClick={cerrarSesion} id='btnCerrarSesion'><i className="fa-solid fa-right-from-bracket"/></button>
+                            <button className='p-0 mt-2 fs-5 fs-sm-4' onClick={cerrarSesion} id='btnCerrarSesion'><i className="fa-solid fa-right-from-bracket"/></button>
                         </li>
                     </ul>
                 </div>
--- src/pages/Inicio.js
+++ src/pages/Inicio.js
@@ -12,24 +12,24 @@ export default function Inicio() {
         <div className="container-fluid">
             <div className="row">
                 <div className="col-12 p-0">
-                    <img src={img_inicial} className="img-fluid w-100" height={'250px'} alt="..." />
+                    <img src={img_inicial} className="img-fluid"  alt="Imagen_aficion" />
                 </div>
             </div>
-            <div className="row text-center" style={{ borderTop: "0.4rem solid rgb(252, 224, 179)" }}>
-                <div className="col-4 p-2" style={{ backgroundColor: "#182E3E", color: "white" }} >
-                    <h3 className="p-1">Echa un vistazo a las plantillas de todos los equipos</h3>
-                    <Link to="/ver_jugadores"><img src={jugadores_andando} className="w-100" height={'78%'} alt="Players waking" /></Link>
+            <div className="row text-center" style={{ borderTop: "0.2rem solid rgb(252, 224, 179)" }}>
+                <div className="col-12 col-md-4 col-lg-4 p-2" style={{ backgroundColor: "#182E3E", color: "white" }} >
+                    <h4 className="p-0 p-sm-1">Echa un vistazo a las plantillas de todos los equipos</h4>
+                    <Link to="/ver_jugadores"><img src={jugadores_andando} className="img-fluid" height={'78%'} alt="Players waking" /></Link>
                  </div>
-                <div className="col-4 p-2" style={{ backgroundColor: "white", color: "#182E3E" }}> 
-                    <h3 className="p-1">Las estadísticas de tus jugadores favoritos en un solo click </h3>
-                    <img src={player_celebrating} className="w-100" height={'78%'} alt="Football pitch" />
+                <div className="col-12 col-md-4 col-lg-4 p-2" style={{ backgroundColor: "white", color: "#182E3E" }}> 
+                    <h4 className="p-0 p-sm-1">Las estadísticas de tus jugadores favoritos en un click </h4>
+                    <img src={player_celebrating} className="img-fluid" height={'78%'} alt="Player celebrating" />
                 </div>
-                <div className="col-4 p-2" style={{ backgroundColor: "#182E3E", color: "white" }}>                    
-                    <h3 className="p-1">Consulta la clasificación y mantente actualizado/a en todo momento </h3>
-                    <img src={football_pitch} className="w-100" height={'78%'} alt="Football pitch" />
+                <div className="col-12 col-md-4 col-lg-4 p-2" style={{ backgroundColor: "#182E3E", color: "white" }}>                    
+                    <h4 className="p-0 p-sm-1">Consulta la clasificación y mantente actualizado/a en todo momento </h4>
+                    <img src={football_pitch} className="img-fluid" height={'78%'} alt="Football pitch" />
                 </div>
             </div>
-            <div className="row" style={{ borderTop: "0.4rem solid rgb(252, 224, 179)" }}><Footer /></div>
+            <div className="row" style={{ borderTop: "0.2rem solid rgb(252, 224, 179)" }}><Footer /></div>
         </div>
     );
 }
